231de8c0a44d9db0b661c04dcfabf93d08a4afcc
Maik Ploigt <maik.ploigt@freiheit.com>
2021-05-01 13:45:29 +0200

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    content: [
+      "./themes/freiheit-com/layouts/**/*.html",
+      "./layouts/**/*.html",
+      "./content/**/*.md",
+      "./content/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
