2635c83ed8a69d9b246cd54fc8929e8206ae4a02
Maik Ploigt <maik.ploigt@freiheit.com>
2022-02-03 12:45:32 +0100

    Fingerprint css and js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: false, // todo: only in production with "NODE_ENV" set to "production"
+    enabled: true, // todo: only in production with "NODE_ENV" set to "production"
     content: [
       "./themes/freiheit-com/layouts/**/*.html",
       "./layouts/**/*.html",
