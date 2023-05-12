87b3878ab4d6cbf9c8761dafcc97d56d5702c895
Maik Ploigt <maik.ploigt@freiheit.com>
2023-05-12 12:04:01 +0200

    Remove not needed javascript listener

--- assets/js/_navigation.js
+++ assets/js/_navigation.js
@@ -1,19 +1,5 @@
 const checkbox = document.getElementById('open-menu-checkbox')
 
-document.body.addEventListener("click", function (evt) {
-    let clickedInNavigation = false;
-    evt.path.forEach(item => {
-        if (item.id) {
-            if (item.id.indexOf('navigation') !== -1) {
-                clickedInNavigation = true;
-            }
-        }
-    });
-    if (!clickedInNavigation) {
-        checkbox.checked = false;
-    }
-}, {passive: true});
-
 document.getElementById('navigation_close').addEventListener("click", function (e) {
     e.preventDefault()
     checkbox.checked = false;
