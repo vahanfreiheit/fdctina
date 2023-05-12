bd5241e42d06f7e3658bec194712fa896053dee3
Maik Ploigt <maik.ploigt@freiheit.com>
2023-05-12 14:13:52 +0200

    Deploy script update

--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,80 @@
 #!/bin/bash
 
+C_R='\033[0;32m' # green
+C_0='\033[0m' # no Color
+
 # removed and generated docs directory
 # and pushes the changes to production/staging
 
-# exit script if no parameter was given
-if [ -z "$1" ]; then
-    echo "No parameter given. Please specify 'production' or 'staging'."
-    exit 1
-fi
+while true
+do  
+  prompt=
+  echo -e "${C_R}Do you want to deploy to [l]ive or [s]taging?${C_0}"
+  read -n1 char
+  case "$char" in
+      [Ll])
+          ENV='production'
+          break
+          ;;
+      [Ss])
+          ENV='staging'
+          break
+          ;;
+      *)
+          echo -e "${C_R}Wrong input: '$char'.\nPlease only enter l or s.${C_0}"
+          continue
+    esac
+done
 
-# exit script if first parameter was not correct
-if [ "$1" != "production" ] && [ "$1" != "staging" ]; then
-    echo "Invalid parameter given. Please specify 'production' or 'staging'."
-    exit 1
-fi
+echo -e "\n${C_R}What changes have you made?${C_0}"
+while true; do
+    read commit_message
+    if [[ -z "$commit_message" ]]; then
+        echo -e "${C_R}Please tell me what you have changed.${C_0}"
+    else
+        break
+    fi
+done
+
+BRANCH_NAME=$(date +"%Y%m%d_%H%M%S")
+
+# create new branch with current date and time as name
+echo -e "\n${C_R}Creating new branch...${C_0}"
+git checkout -b $BRANCH_NAME refs/heads/_$ENV
+
+echo -e "${C_R}Generating docs directory for ${ENV}...${C_0}"
 
 # delete docs directory
-echo "Removing docs directory..."
 rm -rf docs
 
 # generate docs directory
-echo "Generating docs directory for $1..."
-hugo -e $1
+hugo -e $ENV
 
 # check if there are changes in git, create new jobs page with hugo and commit the changes
 if [[ $(git status --porcelain) ]]; then
   # commit changes
-  echo "Committing changes..."
+  echo -e "${C_R}Committing changes...${C_0}"
   git add .
-  git commit -m "Update docs"
+  git commit -m "$commit_message"
 
   # git push to variable git_branch
-  echo "Pushing changes to $1..."
+  echo -e "${C_R}Pushing changes to $ENV...${C_0}"
   # set variable "git_branch" according to first parameter
-  if [ "$1" = "production" ]; then
-      git push origin refs/heads/origin
-  elif [ "$1" = "staging" ]; then
-      git push staging refs/heads/staging
+  git push $ENV refs/heads/$BRANCH_NAME
+  
+  echo -e "${C_R}Opening the browser so you can create the pull request...${C_0}"
+  if [ "$ENV" = "production" ]; then
+    open "https://github.com/freiheit-com/freiheit-com.github.io"
+  else
+    open "https://github.com/mploigt/freiheit"
   fi
 else
-  echo "No changes detected. Nothing to commit."
+  echo -e "${C_R}No changes detected. Nothing to commit.${C_0}"
 fi
-echo "Done!"
+
+# archiving branch
+echo -e "${C_R}Cleaning up...${C_0}"
+git checkout "_$ENV"
+git branch -m $BRANCH_NAME archive/$ENV/$BRANCH_NAME
+
+echo -e "${C_R}Done!${C_0}"
